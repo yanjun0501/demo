ec6371c1b9aca8efb74a20768f2566da4d87dff5
yanjun0501 <liyanjun@ustil.cn>
2017-09-29 22:18:48 +0800

    dom-practice-1

---
+++ admin-dom练习/index.js
@@ -0,0 +1,10 @@
+var t1 = document.getElementById('t1');
+var t2 = document.getElementById('t2');
+
+
+
+t2.onclick = function a(){
+    var s = document.getElementById('s');
+    console.log(s.src);
+    s.src = "http://www.ustil.cn"
+}
